daf147f1261530ed47f5e37340f7bce2d57866f1
rnayak1 <rohitn116@gmail.com>
2020-05-22 12:59:38 +0530

    added cancel button handler in task

--- functions/tasks.js
+++ functions/tasks.js
@@ -1,9 +1,20 @@
 // these functions will interact with task management related functions
 exports.closeModal = (thisObj) => {
     thisObj.setState((prevState) => {
+        // this is prev state
         console.log(prevState)
         const newState = {
-            showModal: false
+            showModal: false,
+            taskHeading: '',
+            taskDetail: '',
+            category: null,
+            taskStatus: null,
+            dueDate: null,
+            isHeaderSet: false,
+            isDesSet: false,
+            isTypeSet: false,
+            isStatusSet: false,
+            isDateSet: false
         }
         return newState;
     });
